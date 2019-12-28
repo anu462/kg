15126abd5250f88bd94c31b1d294252c42699cb5
AKASH <AKASH@DESKTOP-0IRI3LA>
2019-12-28 12:35:32 +0530

    design

---
+++ src/anu/dummy.java
@@ -0,0 +1,11 @@
+package anu;
+
+public class dummy 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello");
+		
+	}
+
+}
---
+++ src/anu/dummy2.java
@@ -0,0 +1,11 @@
+package anu;
+
+public class dummy2 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hi");
+		
+	}
+
+}
